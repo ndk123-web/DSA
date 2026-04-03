367d33ca0b6f1fc088fbf006eb88af122f0ec044
Navnath Kadam <navnathkadam284@gmail.com>
2026-04-03 13:13:17 +0530

    feat: added dijkstra algo

--- Advance Algorithms/Graphs/djikstra.cpp
+++ Advance Algorithms/Graphs/djikstra.cpp
@@ -1,48 +1,65 @@
 #include <iostream>
 #include <vector>
-#include <tuple>
 #include <queue>
+#include <string>
 
-using namespace std;
-
-class Dijsktra
-{
-public:
-    vector<tuple<int, int, int>> edges;
-
-    vector<vector<int>> algoRun(vector<tuple<int, int, int>> &edges, int startNode, int totalNodes)
-    {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-        vector<int> dist(totalNodes, INT_FAST32_MAX);
-
-        // added startNode with distance
-        pq.push({0, startNode});
-
-        while (!pq.empty())
-        {
-
-            // here edge is [from, to, weight]
-            for (auto &edge : edges)
-            {
-                
-            }
+class Dijsktra {
+    private:
+        string name;
+    public:
+        Dijsktra(string name) {
+            this->name = name;
         }
-    }
+
+        vector<int> runDijkstra(vector<vector<int>>& edges, int src, int totalNodes) {
+
+            // min heap
+            priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+            vector<int> distances(totalNodes, INT_MAX);
+            unordered_map<int, vector<pair<int,int>>> adjList;
+
+            for (auto& edge : edges) {
+                int u = edge[0];
+                int v = edge[1];
+                int w = edge[2];
+
+                adjList[u].push_back({v,w});
+            }
+
+            distances[src] = 0;
+            pq.push({0,src});
+
+            while (!pq.empty()) {
+                auto& [dist, node] = pq.top();
+                pq.pop();
+
+                for (auto& edge : edges[node]) {
+                    int neigbour = edge[1];
+                    int weight = edge[2];
+
+                    if (dist + weight < distances[neigbour]) {
+                        distances[neigbour] = dist + weight;
+                        pq.push({distances[neigbour], neigbour});
+                    }
+                }
+            }
+
+            return distances
+        }
 };
 
-int main()
-{
-    vector<tuple<int, int, int>> edges = {
-        {0, 1, 3},
-        {1, 2, 5},
-        {0, 3, 2},
-        {2, 3, 5},
-        {1, 3, 3},
-        {0, 3, 10}};
+int main () {
 
-    Dijsktra dij;
-    int startNode = 0;
-    int totalNodes = 4;
-    vector<vector<int>> ans = dij.algoRun(edges, startNode, totalNodes);
+    vector<vector<int>> edges = {
+        {0,1,1},
+        {0,2, 4},
+        {0, 3, 5},
+        {1, 2, 1},
+        {2,3, 1};
+    }
+    Dijsktra* dij("ndk");
+
+    dij->runDijkstra(edges, 0, 4);    
+
     return 0;
 }
